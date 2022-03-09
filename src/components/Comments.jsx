5cb2e5b02a56d6372815f3589b0b5264c3e78816
BillyZen <willjayandtay@hotmail.co.uk>
2022-03-09 11:41:53 +0000

    condition for no comments

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -4,7 +4,7 @@ import {useState, useEffect} from "react"
 export default function Comments ({id}) {
 
     const [comments, setComments] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
+    const [isLoading, setIsLoading] = useState(false)
 
     useEffect(() => {
             api.getComments(id)
@@ -18,7 +18,7 @@ export default function Comments ({id}) {
     return ( isLoading ? <p>Loading comments...</p> : (
         <div>
             <ul className="article-comments">
-                {comments.map(comment => {
+                {comments.length === 0 ?  <p>Be the first to comment!</p> : comments.map(comment => {
                 return (
                     <li key={comment.comment_id}>
                         <div className="comment-txt">
